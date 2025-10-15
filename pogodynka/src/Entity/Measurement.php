83fa5bc733e7bf4f90ecc6a402310b65fd235663
Wojciech Garycki <168677996+WojGar@users.noreply.github.com>
2025-10-15 22:17:58 +0200

    lab C

---
+++ pogodynka/src/Entity/Measurement.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeasurementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeasurementRepository::class)]
+class Measurement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'measurements')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $measurement_date = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 0)]
+    private ?string $temperature = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 0)]
+    private ?string $humidity = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2)]
+    private ?string $pressure = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getMeasurementDate(): ?\DateTime
+    {
+        return $this->measurement_date;
+    }
+
+    public function setMeasurementDate(\DateTime $measurement_date): static
+    {
+        $this->measurement_date = $measurement_date;
+
+        return $this;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(string $temperature): static
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?string
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(string $humidity): static
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getPressure(): ?string
+    {
+        return $this->pressure;
+    }
+
+    public function setPressure(string $pressure): static
+    {
+        $this->pressure = $pressure;
+
+        return $this;
+    }
+}
